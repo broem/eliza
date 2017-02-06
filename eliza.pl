f1b2e29285578f7552db4b4cccda1016c07fed35
broem <wacbleach@gmail.com>
2017-02-06 00:35:38 -0500

    mostly working, fixed lots

--- eliza.pl
+++ eliza.pl
@@ -38,42 +38,74 @@ print "$eliza Ok $user how can the state help you today?\n";
 while(1){
 	print "[$user] ";
 	my $randomNum = 1 + int rand(10); #random number for automated responses
-	my $input = <>;
+	my $input = "";
+	$input = <>;
 	my $additional="";
-	my $bool=1;
-	my $changed=1;
-	chomp $input;
-	$input = rmwhite($input);
-	#remove the trailing and preceeding whitespace
-	$input = lc $input;
+	my $notFound = 1; 		# if no match default response
+	my $changeBypass = 1; 	# when there is a preloaded response that has a key word, this will bypass
+	my $noQmark = 1;
 
-	if($input=~m/^please eliza, kind and benevolent overlord, dismiss me?$/){
-		print "$eliza I'm glad to see you recognize my greatness, farewell.";
-		last;
+	
+	chomp $input;
+	$input = rmwhite($input); 		#remove the trailing and preceeding whitespace
+	
+	$input = lc $input;		# easier to work with lowercase
+	
+	my $len = length($input);
+
+	# remove and replace Pronouns to change later
+	$input=~s/\byou are\b/bleach5/g;
+	$input=~s/\byou\b/bleach1/g;
+	$input=~s/\byour\b/bleach2/g;
+	$input=~s/\byours\b/bleach3/g;
+	$input=~s/\byourself\b/bleach4/g;
+
+
+	if($input=~m/\beliza\b/) {
+		$changeBypass = 0;
+		$notFound = 0;
+		if($randomNum < 3) {
+			$input="We're not here to discuss me, got it";
+		}
+		if($randomNum >= 3 and $randomNum <= 6) {
+			$noQmark = 0;
+			$input="This is a place to talk about yourself, let's keep it that way."
+		}
+		if($randomNum > 6) {
+			$input="Listen, last time I checked I wasn't the one in need of a psyc, don't talk about me."
+		}
 	}
 
 	$input=~s/[.?!]//;
-	#remove some colloquial things
-	if(length($input) ge 5) {
-	$input=~s/^(\bok\b|\byeah\b|\byes\b|\bno\b|\bwell\b)//;
+	#remove some beginning colloquial things
+	if($len > 5) {
+	$input=~s/^(\bok\b|\byeah\b|\byes\b|\bno\b|\bwell\b)//g;
 	}
 
-	# if(length($input) ge 40) {
-	# 	$input = "Either you shorten your sentences or I'm leaving.\n I do not get paid to read these long diatribes.";
-	# 	$bool = 0;
-	# 	$changed = 0;
-	# }
-
-	if($input=~m/sorry|apologize/ and $changed eq 1) {
-		$bool = 0;
-		$input = "No need to apologize. Can you continue";
+	if(length($input) > 45) {
+		$input = "I do not get paid to read these long diatribes, let's keep it short ok";
+		$notFound = 0;
+		$changeBypass = 0;
 	}
 
-	#why dont we start from the beginning
+	if($input=~m/\bguess\b/g){
+		$input = "you guess";
+		$notFound = 0;
+		$changeBypass = 0;
+	}
 
-	#make eliza so unhappy she quits!
-	if($input=~m/^(\byes\b|\bno\b|\bnope\b|\byep\b|\byeah\b)$/ and $changed eq 1){
-		$bool = 0;
+	if($input=~m/sorry|apologize/ and $changeBypass == 1) {
+		$notFound = 0;
+		$changeBypass = 0;
+		$noQmark = 0;
+		$input = "Don't apologize. Continue.";
+	}
+
+	# why dont we start from the beginning
+
+	# make eliza so unhappy she quits
+	if($input=~m/^(\byes\b|\bno\b|\bnope\b|\byep\b|\byeah\b)$/ and $changeBypass == 1){
+		$notFound = 0;
 		if($randomNum <= 5 and $annoyance < 3) {
 			$input= "...could you elaborate"; 
 			$annoyance++;}
@@ -90,65 +122,128 @@ while(1){
 		}
 	}
 
-	if($input=~m/\b(i am)\b|\b(im)\b|\b(i'm)\b/g and $changed eq 1) {
-		$bool = 0;
-		$input=~s/$&/you're/g;
+
+	# Pronoun section 
+	if($changeBypass == 1) {
+
+		if($input=~m/\b(i am)\b|\b(im)\b|\b(i'm)\b/g) {
+			$notFound = 0;
+			$input=~s/$&/you're/g;
+		}
+
+		if($input=~m/\bmy\b/) {
+			$notFound = 0;
+			$input=~s/$&/your/g;
+		}
+		
+		if($input=~m/(\bi\b|\bme\b)/g) {
+			$notFound = 0;
+			$input=~s/\b$&\b/you/g;
+		}
+
+		if($input=~m/\bmyself\b/) {
+			$notFound = 0;
+			$input=~s/\b$&\b/yourself/g;
+		}
+
+		# replacing the ones removed earlier
+		if($input=~m/bleach/) {
+			$input=~s/\bbleach5\b/I am/g;
+			$input=~s/\bbleach1\b/I/g;
+			$input=~s/\bbleach2\b/my/g;
+			$input=~s/\bbleach3\b/mine/g;
+			$input=~s/\bbleach4\b/myself/g;
+			$notFound = 0;
+		}
+
 	}
-	
-	if($input=~m/(\bi\b|\bme\b|\bmy\b)/g and $changed eq 1){
-		$bool = 0;
-		$input=~s/\b$&\b/you/g;
+	# end of section
+
+	if($input=~m/\bleave\b|\bquit\b|\b"end this"\b/) {
+		$additional = "If you want to leave just say quit.";
 	}
 
-	if($input=~m/\bbecause\b/ and $changed eq 1){
+	if($input=~m/^(hm+)$/) {
+		$noQmark = 0;
+		$input = "My time is valuable, don't waste it.";
+	}
+
+	if($input=~m/^\bbecause\b/ and $changeBypass == 1){
 		$input=~s/$&//g;
 	}
 
 	if($input=~m/\bfeel\b/g){
-		$bool = 0;
+		$notFound = 0;
 		$input = "Why do you think you feel this way";
 	}
 
-	if($input=~m/\b(yes|ok)\b/g and $changed eq 1){
-		$bool = 0;
+	# remove yes and ok
+	if($input=~m/\b(yes|ok)\b/g and $changeBypass == 1){
+		$notFound = 0;
 		$input=~s/$&//;
 	}
 
-	#happy line of questioning
+	# happy line of questioning
 	if($input=~m/\bhappy\b|\bglad\b|\bjoyful\b/){
-		$input = "Atleast someone is $& to be here, can we move on";
-		$bool = 0;
+		$additional = "Whoopty doo! What else?";
+		$notFound = 0;
 	}
 
 	if($input=~m/\bangry\b|\bsad\b|\bupset\b|\bdepressed\b|\banxious\b/) {
+		$notFound = 0;
 		$input= "its ok to be $&, tell me more.";
 	}
 
 	if($input=~m/\bhelp\b/) {
+		$notFound = 0;
 		$input = "ok...what do you think I can do";
 	}
 
+	if($input=~m/\+|\-|\=|\*/) {
+		$noQmark = 0;
+		$notFound = 0;
+		$input = "If this is math, I'm not a calculator. Act responsibly or I WILL cut your time short!";
+	}
+
 	if($input=~m/^quit$/){
+		#sleep(2);
 		print "$eliza Finally! I don't get paid enough for this, bye.";
 		last;
 	}
 
-
-	if($bool eq 1 ){
-		#if doesnt ultimately understand
+	#if doesnt ultimately understand
+	if($notFound == 1){
 		$said = $input;
-		$input="I don't know what $said means, explain";
+		$noQmark = 0;
+		if($randomNum < 3) {
+			$input="I don't know what $said means, explain.";
+		}
+		if($randomNum > 3 and $randomNum < 6) {
+			$input="I'm not following you, can you explain differently.";
+		}
+		if($randomNum >= 6){
+			$input="English is not my first language, reword that.";
+		}
+
 		#$input="I think you're meds are wearing off...can you try again"
 	}
 	# things to remove: because, 
 	#i dont get paid enough to play these games
 
+	
+
 	$input = rmwhite($input);
+	if($input eq ""){
+		$input = "well";
+	}
+
+
+
 	$input = ucfirst $input;
-	if($input ne "" or !$input=~m/\./)
-	{
+	if($noQmark == 1) {
 		$input .= "?"
 	}
+	#sleep(2);
 	print "$eliza $input $additional\n";
 }
 
